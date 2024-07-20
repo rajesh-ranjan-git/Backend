dafdd6610c2e9e0510f13e64c5132a1ae4b49483
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2024-07-21 04:38:23 +0530

    User Schema

--- Models/user.model.js
+++ Models/user.model.js
@@ -1,1 +1,19 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
+
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema({
+  userName: { type: String, unique: true, lowercase: true, required: true },
+  email: { type: String, unique: true, lowercase: true, required: true },
+  password: { type: String, required: true, min: [8, "Password is too short"] },
+  phoneNumber: {
+    type: Number,
+    required: true,
+    min: [10, "Not a valid number"],
+    min: [12, "Not a valid number"],
+  },
+});
+
+const userModel = model("User", userSchema);
+
+export default userModel;
