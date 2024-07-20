75376a54cba324f61288852cec046e7af5f3aebf
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2024-07-21 04:25:17 +0530

    DB Connection

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 import express from "express";
+import mongoose from "mongoose";
+import "dotenv/config";
 import productsRouter from "./Routes/products.routes.js";
 import usersRouter from "./Routes/users.routes.js";
 
+// We will include this line in .env file and should not upload it to git or anywhere for security purposes.
+// let mongoDbURI =
+//   "mongodb+srv://rajesh-mongo:rajesh@getkart.1j7n22q.mongodb.net/?retryWrites=true&w=majority&appName=GetKart";
+
+console.log(process.env.MONGODB_URI);
+
 const server = express();
-const port = 3000;
+let port = process.env.PORT; // We should use let because port may be busy on server do server will modify/assign a different port and may cause trouble if we are using const here.
 
 server.use(express.json());
+
+const dbConnect = async () => {
+  return await mongoose.connect(process.env.MONGODB_URI);
+};
+
+dbConnect()
+  .then(() => {
+    console.log("DB Connected");
+    server.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`);
+    });
+  })
+  .catch(() => {
+    console.log("Unable to connect to DB.");
+  });
 
 //CRUD in REST
 
@@ -37,6 +60,6 @@ server.use(express.json());
 server.use("/products", productsRouter);
 server.use("/users", usersRouter);
 
-server.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+// server.listen(port, () => {
+//   console.log(`Server is running on http://localhost:${port}`);
+// });
